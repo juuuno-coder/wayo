77893b9cd2edbb0e2d6c5fb4a27ecf61aac4ed29
admin <admin@adminui-MacBookPro.local>
2026-01-08 15:02:53 +0900

    Fix CORS: add wayo.co.kr domain to allowed origins

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,12 +1,19 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins(*(
-      [ENV['FRONTEND_URL'], 'http://localhost:3400', 'http://127.0.0.1:3400', 'http://localhost:3000', 'https://wayo.co.kr', 'https://www.wayo.co.kr', 'http://wayo.co.kr', 'http://wayo.co.kr:3400', 'http://gabojago.wayo.co.kr', 'https://gabojago.wayo.co.kr', 'http://gabojago.wayo.co.kr:3400'].compact
-    ))
+    origins [
+      'http://localhost:3000',
+      'https://localhost:3000',
+      'https://wayo.co.kr',
+      'https://www.wayo.co.kr',
+      'https://*.wayo.co.kr',
+      /https:\/\/.*\.vercel\.app$/,
+      /https:\/\/wayo-.*\.vercel\.app$/
+    ]
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization'] # JWT 토큰 헤더 노출 허용
+      credentials: true,
+      expose: ['Authorization']
   end
 end
